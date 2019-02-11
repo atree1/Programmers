e4e14b8ca30df28228b88fcaa8c1f399bf6f0980
atree1 <lhj4109816@gmail.com>
2019-02-11 12:25:59 +0900

    프로그래머스 문제

---
+++ Programmers/src/exam/Main.java
@@ -0,0 +1,18 @@
+package exam;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Solution s=new Solution();
+		String[] parti= {"leo", "kiki", "eden"};
+		String[] completion= {"kiki", "eden"};
+		
+		System.out.println(s.solution(parti, completion));
+		
+		
+
+	}
+	
+}
